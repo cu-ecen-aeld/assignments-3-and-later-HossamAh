13889e0b2091ca1fc12d1c292a5f12baaaa981b5
Hossam Ahmed <hossamahmed201515@gmail.com>
2024-04-04 05:41:17 +0200

    system calls TODOS done

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#define _XOPEN_SOURCE
 bool do_system(const char *command);
 
 bool do_exec(int count, ...);
